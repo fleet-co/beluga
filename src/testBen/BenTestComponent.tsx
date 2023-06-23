195da436c820c5c89097b0872380223eb8c95079
Leandro <leandro.braga@fleet.co>
2023-06-23 11:54:29 +0200

    Update BenTestComponent.tsx

--- src/testBen/BenTestComponent.tsx
+++ src/testBen/BenTestComponent.tsx
@@ -1,13 +1,13 @@
 
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://npeoynokbejbzowsvxpr.supabase.co';
+const supabaseUrl = "https://npeoynokbejbzowsvxpr.supabase.co";
 // const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZW95bm9rYmVqYnpvd3N2eHByIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NzUwNzY5NCwiZXhwIjoyMDAzMDgzNjk0fQ.pznBmmKTnFYPXYX85d124Wk8yiNN6g1E7QJIHUSZaS4';
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZW95bm9rYmVqYnpvd3N2eHByIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NzUwNzY5NCwiZXhwIjoyMDAzMDgzNjk0fQ.pznBmmKTnFYPXYX85d124Wk8yiNN6g1E7QJIHUSZaS4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const { data: pages, error } = await supabase
-  .from('pages')
+  .from("pages")
   .select(`
     id,
     name,
@@ -16,6 +16,7 @@ const { data: pages, error } = await supabase
 
 const TestBenComponent = () => {
   console.log(pages);
+
   return pages?.map(page => <>{page.name} : {page.slug}</>);
 };
 
