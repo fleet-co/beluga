83549eec3bdb6f2479ed3b4b5ff9b9a82e6b08c8
Benjamin Brizzi <benjamin@fleet.co>
2023-06-23 11:34:51 +0200

    Database connexion

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BenTestComponent from './testBen/BenTestComponent';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <BenTestComponent />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
