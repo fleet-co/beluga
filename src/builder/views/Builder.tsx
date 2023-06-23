c6b9c068f0a935aa52fa8b950e20c030a83f5f2a
Lorys <lorys.aveneau@fleet.co>
2023-06-23 14:21:37 +0200

    Better select

--- src/builder/views/Builder.tsx
+++ src/builder/views/Builder.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 import Button from "@mui/material/Button";
 import SelectBlockDialog from "../../components/SelectBlockDialog";
+import { BlockData } from "../../blocks";
 
 const App = () => {
   const [blocks, setBlocks] = useState<any>([]);
   const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
 
+  const handleClose = (value: BlockData | undefined) => {
+    setDialogOpen(false);
+    if (value) {
+      addBlock(value);
+    }
+  };
+
+  const addBlock = (block: BlockData) => {
+    console.log(block)
+    setBlocks([...blocks, block.component]);
+  }
   return (
     <>
       Bonjour jeune site builder
@@ -13,7 +25,7 @@ const App = () => {
       <Button variant="contained" onClick={() => setDialogOpen(true)}>Choisir un block</Button>
       <SelectBlockDialog
         open={isDialogOpen}
-        onClose={() => setDialogOpen(false)}
+        onClose={handleClose}
       />
       {blocks}
     </>
