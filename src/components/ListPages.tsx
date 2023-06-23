7e0c8172d0bc0a762141bece97d0329127d3c793
Leandro <leandro.braga@fleet.co>
2023-06-23 16:38:24 +0200

    theme done

--- src/components/ListPages.tsx
+++ src/components/ListPages.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import SupabaseService from "../tools/SupabaseClient";
 import { Page } from '../types/types';
 
-import { Link } from '@mui/material';
-import { Timeline, TimelineItem, TimelineDot, TimelineSeparator, TimelineConnector, TimelineContent } from '@mui/lab';
+import { Link } from "@mui/material";
+import { Timeline, TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from "@mui/lab";
+import AbcIcon from "@mui/icons-material/Abc";
 
 const ListPages = () => {
   const [pages, setPages] = useState<Page[]>([]);
 
   useEffect(() => {
     const sbs = new SupabaseService();
+
     sbs.getPages().then((p: any) => {
       setPages(p.data);
     });
@@ -20,11 +22,17 @@ const ListPages = () => {
       {pages?.map(page =>
         <TimelineItem>
           <TimelineSeparator>
-            <TimelineDot />
+            <TimelineDot>
+              <AbcIcon />
+            </TimelineDot>
             <TimelineConnector />
           </TimelineSeparator>
-          <TimelineContent><Link href={`page/${page.slug}`} underline="none">{page.name}</Link></TimelineContent>
-        </TimelineItem>
+          <TimelineContent>
+            <Link href={`page/${page.slug}`} underline="none">
+              {page.name}
+            </Link>
+          </TimelineContent>
+        </TimelineItem>,
       )}
     </Timeline>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import RoutesComponent from "./routes";
+import theme from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RoutesComponent />
+    <ThemeProvider theme={theme}>
+      <RoutesComponent />
+    </ThemeProvider>
   </React.StrictMode>,
 );
