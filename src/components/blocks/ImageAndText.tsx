adeea8890d5dcd32f80e116190e08bf60c91d77b
NellyBP <68547260+NellyBP@users.noreply.github.com>
2023-06-23 17:03:04 +0200

    Update ImageAndText.tsx

--- src/components/blocks/ImageAndText.tsx
+++ src/components/blocks/ImageAndText.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
 
 interface ImageAndTextProps {
   image: string,
@@ -8,26 +9,34 @@ interface ImageAndTextProps {
 
 const ImageAndText = (props: ImageAndTextProps) => {
   const { text, image } = props;
-
+  const defautlImg = "https://s2.qwant.com/thumbr/0x380/7/f/dac2957cddf977d609ce87dc527188c2c16fc68630d47e7e3236b0462ad709/Belugas-formam-sociedades-complexas.jpg?u=https%3A%2F%2Fsocientifica.com.br%2Fwp-content%2Fuploads%2F2020%2F07%2FBelugas-formam-sociedades-complexas.jpg&q=0&b=1&p=0&a=0"
   return (
     <Box
       sx={{
         width: "100vw",
         display: "flex",
-        height: 300,
-        backgroundColor: "primary.dark",
+        height: 400,
+        backgroundColor: "primary.light",
       }}
     >
       <Box
         sx={{
-          width: "100vw",
+          width: "90vw",
           display: "flex",
           flexDirection: "row",
+          margin: "auto",
+          gap: "24px",
+          height: "90%",
         }}
       >
-        {text}
+        <Typography component="p" sx={{
+          fontSize: "1.5rem",
+          width: "50%",
+        }}>
+          {text || "lorum ipsum dolor con la visita de la sagrada familia"}
+        </Typography>
+        <img src={image || defautlImg} style={{ "height": "100%", "width": "auto", "borderRadius": "4px" }} />
       </Box>
-      <img src={image} />
     </Box>
   );
 };
