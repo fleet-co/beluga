565b1d3979eae7d618bfe00617b22929f2a3586f
Benjamin Brizzi <benjamin@fleet.co>
2023-06-23 12:06:42 +0200

    DB Client

--- src/testBen/BenTestComponent.tsx
+++ src/testBen/BenTestComponent.tsx
@@ -1,22 +1,30 @@
+import { useEffect, useState } from 'react';
+import SupabaseService from "../tools/SupabaseClient";
 
-import { createClient } from '@supabase/supabase-js';
+interface Block {
+  id: number;
+  order: number;
+  slug: string;
+  type: string;
+  contents: any;
+}
+interface Page {
+  id: number;
+  name: string;
+  slug: string;
+  blocks: Block[];
 
-const supabaseUrl = 'https://npeoynokbejbzowsvxpr.supabase.co';
-// const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZW95bm9rYmVqYnpvd3N2eHByIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NzUwNzY5NCwiZXhwIjoyMDAzMDgzNjk0fQ.pznBmmKTnFYPXYX85d124Wk8yiNN6g1E7QJIHUSZaS4';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-const { data: pages, error } = await supabase
-  .from('pages')
-  .select(`
-    id,
-    name,
-    slug,
-    blocks (id, order, type, contents)`);
-
+}
 const TestBenComponent = () => {
-  console.log(pages);
-  return pages?.map(page => <>{page.name} : {page.slug}</>);
+  const [pages, setPages] = useState<Page[]>([]);
+  useEffect(() => {
+    const sbs = new SupabaseService();
+    sbs.getPages().then((p: any) => {
+      setPages(p.data);
+    });
+  }, []);
+
+  return pages?.map((page: Page) => <>{page.name} : {page.slug}</>);
 };
 
 export default TestBenComponent;
