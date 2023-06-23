897bef4f851e82848881173bdc44bac5e7d029b6
Leandro <leandro.braga@fleet.co>
2023-06-23 11:40:31 +0200

    eslint config + mui icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import BenTestComponent from './testBen/BenTestComponent';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </head>
       <BenTestComponent />
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -31,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
