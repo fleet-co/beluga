3fb66e3518b84d7a0ad3b88b3cc7787057e536b8
Simon Lemaitre <simon.lemaitre@fleet.co>
2023-06-23 14:15:37 +0200

    cc

--- src/builder/views/Builder.tsx
+++ src/builder/views/Builder.tsx
@@ -1,16 +1,37 @@
-import { useState } from "react";
-import Button from "@mui/material/Button";
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './Builder.css';
 
-const App = () => {
+function Builder() {
   const [blocks, setBlocks] = useState<any>([]);
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+
+  const newDiv = (title: string, description: string) => {
+    return (
+      <div className="newDivContainer" sx={{}}>
+        <h4>
+          {title}
+        </h4>
+        <p>
+        {description}
+        </p>
+      </div>
+    )
+  };
 
   return (
-    <>
-      Bonjour jeune site builder
-      <Button variant="contained" onClick={() => setBlocks([...blocks, <div>e</div>])}>Ne pas cliquer</Button>
+    <div className="builderContainer">
+      <div className="toolBar">
+        Bonjour jeune site builder
+        <TextField id="standard-basic" label="Titre" variant="standard" onChange={(e: any) => setTitle(e.target.value)} />
+        <TextField id="standard-basic" label="Description" variant="standard" onChange={(e: any) => setDescription(e.target.value)} />
+        <Button variant="contained" onClick={() => setBlocks([...blocks, newDiv(title, description)])}>Ne pas cliquer</Button>
+      </div>
       {blocks}
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default App;
+export default Builder
