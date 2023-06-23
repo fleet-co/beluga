749aa3a6537b13df6366c7f84e38b06918b54b9f
Lorys <lorys.aveneau@fleet.co>
2023-06-23 14:15:31 +0200

    Select Block

--- src/builder/views/Builder.tsx
+++ src/builder/views/Builder.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import Button from "@mui/material/Button";
+import SelectBlockDialog from "../../components/SelectBlockDialog";
 
 const App = () => {
   const [blocks, setBlocks] = useState<any>([]);
+  const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
 
   return (
     <>
       Bonjour jeune site builder
       <Button variant="contained" onClick={() => setBlocks([...blocks, <div>e</div>])}>Ne pas cliquer</Button>
+      <Button variant="contained" onClick={() => setDialogOpen(true)}>Choisir un block</Button>
+      <SelectBlockDialog
+        open={isDialogOpen}
+        onClose={() => setDialogOpen(false)}
+      />
       {blocks}
     </>
   );
